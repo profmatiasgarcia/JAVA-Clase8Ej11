046b6d4bcbf84f9ab33368c3a8792c2ca7c50d19
profmatiasgarcia <info@profmatiasgarcia.com.ar>
2022-08-26 22:50:32 -0300

    principal

---
+++ src/main/java/Controllers/LoginServlet.java
@@ -0,0 +1,116 @@
+/**
+ * @author Prof Matias Garcia.
+ * <p> Copyright (C) 2022 para <a href = "https://www.profmatiasgarcia.com.ar/"> www.profmatiasgarcia.com.ar </a>
+ * - con licencia GNU GPL3.
+ * <p> Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la
+ * Licencia Pública General de GNU según es publicada por la Free Software Foundation, 
+ * bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, 
+ * incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO
+ * PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+ * Debería haber recibido una copia de la Licencia Pública General junto con este programa. 
+ * Si no ha sido así ingrese a <a href = "http://www.gnu.org/licenses/"> GNU org </a>
+**/
+package Controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private final String userID = "matias";
+    private final String password = "garcia";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet LoginServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        String curse = request.getParameter("curse");
+
+        if (userID.equals(user) && password.equals(pwd)) {
+            Cookie c1 = new Cookie("user", user);
+            Cookie c2 = new Cookie("curse", curse);
+            //setting cookie to expiry in 30 mins
+            c1.setMaxAge(30 * 60);
+            c2.setMaxAge(30 * 60);
+            response.addCookie(c1);
+            response.addCookie(c2);
+            response.sendRedirect("LoginSuccess.jsp");
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/index.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>ERROR: El usuario y/o contraseña son incorrectos.</font>");
+            rd.include(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/LoginSuccess.jsp
@@ -0,0 +1,41 @@
+<%--
+ * @author Prof Matias Garcia.
+ * <p> Copyright (C) 2022 para <a href = "https://www.profmatiasgarcia.com.ar/"> www.profmatiasgarcia.com.ar </a>
+ * - con licencia GNU GPL3.
+ * <p> Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la
+ * Licencia Pública General de GNU según es publicada por la Free Software Foundation, 
+ * bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. 
+ * Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, 
+ * incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO
+ * PARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
+ * Debería haber recibido una copia de la Licencia Pública General junto con este programa. 
+ * Si no ha sido así ingrese a <a href = "http://www.gnu.org/licenses/"> GNU org </a>
+--%>
+<%@ page language="java" contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "https://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+    <title>Login Success Page</title>
+</head>
+<body>
+    <%
+        String userName = null;
+        String curseSelected = null;
+        Cookie c[] = request.getCookies();
+        if(c !=null){
+            userName = c[0].getValue();
+            curseSelected = c[1].getValue();   
+        }
+        if(userName == null) response.sendRedirect("index.html");
+    %>
+    <h3>Hi <%=userName%>, Login successful at <%=curseSelected%> curse.</h3>
+    <br>
+    <hr>
+    <h4><a href="MainPage.jsp"> Access to MainPage with sesion </a></h4>
+    <hr>
+    <form action="LogoutServlet" method="post">
+        <input type="submit" value="Logout" >
+    </form>
+</body>
+</html>
